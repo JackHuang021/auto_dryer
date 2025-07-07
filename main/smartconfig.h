281ccf8932bf91d3496cf68076f269ee1fbd28d5
Jack Huang <jackhuang021@gmail.com>
2025-07-07 23:52:52 +0800

    move include headers to .c
    Signed-off-by: Jack Huang <jackhuang021@gmail.com>

--- main/smartconfig.h
+++ main/smartconfig.h
@@ -4,20 +4,6 @@
 
 #pragma once
 
-#include "string.h"
-#include "stdlib.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_wifi.h"
-#include "esp_eap_client.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "esp_netif.h"
-#include "esp_smartconfig.h"
-#include "esp_mac.h"
 
 #ifdef __cplusplus
 extern "C" {
